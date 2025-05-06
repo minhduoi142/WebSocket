10f9472defc747d03d60986cfdb8ef3415192352
MinhTran <minhduoi142006@gmail.com>
2025-05-07 06:06:31 +0700

    Update Greeting.java

--- Message/src/main/java/com/Minh/Message/Greeting.java
+++ Message/src/main/java/com/Minh/Message/Greeting.java
@@ -1,18 +1,12 @@
 package com.Minh.Message;
 
+    import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Greeting {
-
-    private String content;
-
-    public Greeting() {
-    }
-
-    public Greeting(String content) {
-        this.content = content;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
+   String content;
 }
